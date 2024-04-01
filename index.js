72f4b777ae12194beeaef5a6fb0a3a4790588d50
GauravPande123webdeveloper <gauravpandeyidforfunctionup@gmail.com>
2024-04-01 12:50:01 +0530

    params

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ const express= require('express');
 const app=express();
 // create router instance for routing from express
 const router= express.Router();
+// enabling json data read
+app.use(express.json());
 // get api creation router.get(path,callback)
-router.post('/hello',function (req,res){
-    res.send('my first api!');
+router.get('/:country/user/:state',function (req,res){
+    // getting values from query params 
+    // const name=req.query.product;
+    // const brand=req.query.brand;
+    // getting query object
+    // const data=req.query;
+    // destructuring the query param
+    const {product,brand}=req.query;
+    // accessing path params
+    const countryName=req.params;
+    // accessing body req
+    const user=req.body;
+    // console.log(countryName,user);
+    console.log(user)
+    res.send({status: 'ok', message:{name:product,brand:brand},user});
 })
 //global middleware for all api calling defined with router
 app.use('/',router)
